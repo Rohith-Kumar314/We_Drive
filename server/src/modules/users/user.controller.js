fe7ecc9b8cc7c89e4a890b0ef8af912492c2af22
Rohith Kumar Asarelly <rohith.asarelly@quadratyx.com>
2026-09-22 16:09:19 +0530

    feat: completed users module

--- server/src/modules/users/user.controller.js
+++ server/src/modules/users/user.controller.js
@@ -1,3 +1,5 @@
+import { AppError } from '../../utils/appError.js';
+import { User } from './user.model.js';
 import * as userService from './user.service.js';
 
 export const getAllUsers = async (req,res) => {
@@ -19,5 +21,27 @@ export const createUser = async (req,res) => {
     res.status(200).json({success:true, message:"User created successfully", data:newUser});
 }
 
-export const updateUser = async (req,res) => {}
-export const deleteUser = async (req,res) => {}
+export const updateUser = async (req,res) => {
+    const {id} = req.params;
+
+    const updateUser = await User.findByIdAndUpdate(id,req.body,{returnDocument:"after", runValidators:true});
+    if(!updateUser){
+        throw new AppError(404,"User not found");
+    }
+
+    res.status(200).json({
+        success:true,
+        message:"User Updated Successfully",
+        data: updateUser
+    });
+}
+
+export const deleteUser = async (req,res) => {
+    // id is already validated at the router using middleware and attached, so no need to validate it again.
+    const id = req.params.id;
+
+    const deletedUser = await User.findByIdAndDelete(id);
+    console.log(deletedUser);
+    // considering idempotency for Delete request, even if the user not found , we are considering it as valid deletion operation only
+    res.status(200).json({success:true, message:"User deleted successfully", data: []}); //instead of sending user we are sending empty array , because when the user is already deleted it becomes null, which makes inconsistent in the UI. 
+}
